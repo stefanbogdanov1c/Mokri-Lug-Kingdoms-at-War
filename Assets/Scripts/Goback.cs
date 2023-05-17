f2eac0ec3e2bca6974e0958199c703b40bfa82be
matijaristanov1c <ristanovicmatija2001@gmail.com>
2023-05-17 21:57:34 +0200

    Dodao dugme za Goback

---
+++ Assets/Scripts/Goback.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Goback : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    public void GoBack()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+    
+}
