014b062f76fc9ed76f553ae694715f49299b0d2e
stefanbogdanov1c <sbogdanovic@synapticon.com>
2023-05-20 21:55:03 +0200

    Fixed by checking if the component if already exists

--- Assets/Scripts/DontDestroy.cs
+++ Assets/Scripts/DontDestroy.cs
@@ -4,9 +4,24 @@ using UnityEngine;
 
 public class DontDestroy : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private static AudioSource firstInstance;
+
     void Awake()
     {
-        DontDestroyOnLoad(this);
+        // Check if the first instance already exists
+        if (firstInstance == null)
+        {
+            // Set the first instance if it doesn't exist
+            firstInstance = GetComponent<AudioSource>();
+
+            // Don't destroy this game object when the scene changes
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            // Destroy any additional instances that are created
+            Destroy(gameObject);
+        }
     }
+
 }
