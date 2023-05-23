8b4f103b3c3fcff9f0abf2ca66cee1e2e5396d83
Dejan Jovanovic <dejanjovanovicofficial@gmail.com>
2023-05-23 17:53:15 +0200

    Scoreboard online

---
+++ RVAS Scoreboard/SolMokriLug/MokriLug/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MokriLug.Startup))]
+namespace MokriLug
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
