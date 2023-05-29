a8c5cca05bff9ca84e171b266359344862f5b693
Dejan Jovanovic <dejanjovanovicofficial@gmail.com>
2023-05-29 21:26:05 +0200

    Check if user exists

--- Assets/Scripts/Write.cs
+++ Assets/Scripts/Write.cs
@@ -1,10 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using MySql.Data.MySqlClient;
 using System;
-using UnityEngine.UI;
 using TMPro;
+using UnityEngine;
 
 public class Write : MonoBehaviour
 {
@@ -14,27 +11,49 @@ public class Write : MonoBehaviour
     private MySqlConnection MS_Connection;
     private MySqlCommand MS_Command;
     string query;
+    string check_user;
     public void sendInfo()
     {
         try
         {
             connection();
 
-            if (username.text=="")
+            if (username.text == "")
             {
                 username.text = "Guest";
             }
 
-            query = "INSERT INTO users ( userName ) VALUES ('" + username.text + "');";
+            check_user = "SELECT COUNT(*) FROM users WHERE userName = '" + username.text.ToLower() + "';";
+
+            MS_Command = new MySqlCommand(check_user, MS_Connection);
+
+            MS_Command.ExecuteScalar();
+
+            var row_number = MS_Command.ExecuteScalar();
+            Debug.Log(Convert.ToInt32(row_number));
+            Debug.Log(row_number.GetType());
+
+            if (Convert.ToInt32(row_number) < 1)
+            {
+
+                query = "INSERT INTO users ( userName ) VALUES ('" + username.text.ToLower() + "');";
 
 
-            MS_Command = new MySqlCommand(query, MS_Connection);
+                MS_Command = new MySqlCommand(query, MS_Connection);
 
-            MS_Command.ExecuteNonQuery();
+                MS_Command.ExecuteNonQuery();
 
-            MS_Connection.Close();
+                MS_Connection.Close();
+            }
+            else
+            {
+                return;
+            }
 
-        }catch (Exception ex)
+
+
+        }
+        catch (Exception ex)
         {
             Console.Write(ex.Message);
         }
