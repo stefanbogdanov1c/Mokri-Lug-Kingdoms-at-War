1d9198c02c0c5bc245477cd10590b22a9670dabd
matijaristanov1c <ristanovicmatija2001@gmail.com>
2023-05-30 20:55:03 +0200

    Obrisano dugme i automatsko ucitavanje podataka

--- Assets/Scripts/Leaderboard.cs
+++ Assets/Scripts/Leaderboard.cs
@@ -8,24 +8,6 @@ public class Leaderboard : MonoBehaviour
     public GameObject options;
     void Start()
     {
-        textCanvas.text = "";
-        query = "SELECT * FROM users ORDER BY (won/lost) DESC";
-
-        connectionString = "Server=localhost; database = mokrilugkaw; user = root; password = ''; charset = utf8";
-
-        connection = new MySqlConnection(connectionString);
-
-        connection.Open();
-
-        command = new MySqlCommand(query, connection);
-
-        reader = command.ExecuteReader();
-
-        while (reader.Read())
-        {
-            textCanvas.text += "\n" + reader[0].ToString() + "       " + reader[1].ToString() + "        " + reader[2].ToString();
-        }
-        reader.Close();
         options.SetActive(false);
     }
 }
